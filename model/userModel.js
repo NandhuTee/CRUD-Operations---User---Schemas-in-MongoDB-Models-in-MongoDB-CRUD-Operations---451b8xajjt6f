a275c013ac0e48c5bd610ebe7e1c4cf1147c2230
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-06-18 10:47:00 +0530

    Update userModel.js

--- model/userModel.js
+++ model/userModel.js
@@ -4,12 +4,11 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true // Adding a required field ensures that a user must have a name
+   
   },
   email: {
     type: String,
-    required: true, // Adding a required field ensures that a user must have an email
-    unique: true // Ensuring that the email is unique in the database
+   
   }
 });
 
