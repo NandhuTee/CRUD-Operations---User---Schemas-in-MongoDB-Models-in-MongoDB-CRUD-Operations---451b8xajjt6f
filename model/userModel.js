ef1b68ff94a59ad44923dcff40a4263f0d7c5856
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-06-18 11:00:31 +0530

    Update userModel.js

--- model/userModel.js
+++ model/userModel.js
@@ -4,11 +4,12 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-   
+    required: true
   },
   email: {
     type: String,
-   
+    required: true,
+    unique: true
   }
 });
 
