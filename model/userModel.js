ad76d14e78a07b9ca1b47100191754ad04a1f69f
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-06-18 09:04:19 +0530

    Update userModel.js

--- model/userModel.js
+++ model/userModel.js
@@ -5,6 +5,13 @@ const userSchema = new mongoose.Schema({
   // Implement the user schema fields:
   // name as String and
   // email as a String,
+  name:{
+    type:String
+  },
+  email:{
+    type:String,
+  }
 });
 
 // Create and export the User model
+module.exports =mongoose.model('User',userScheme);
