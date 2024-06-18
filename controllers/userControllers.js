37e17bd3ac71de064ad9a4e583adc25cd66d2777
Nandhini_T <129613480+NandhuTee@users.noreply.github.com>
2024-06-18 10:44:24 +0530

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -50,7 +50,7 @@ router.patch('/users/:id', async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    res.status(200).json({ message: "User Updated", user });
+    res.status(200).json({ message: "User updated", user });
   } catch (err) {
     res.status(500).json({ message: 'Internal server error' });
   }
@@ -67,7 +67,7 @@ router.delete('/users/:id', async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    res.status(200).json({ message: "User Deleted", user });
+    res.status(200).json({ message: "User deleted", user });
   } catch (err) {
     res.status(500).json({ message: 'Internal server error' });
   }
